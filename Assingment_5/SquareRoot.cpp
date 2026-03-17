f2c565bab71b15e154cb3e168b686e21a7e4b2a8
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-03-18 00:16:44 +0530

    Square root of

---
+++ Assingment_5/SquareRoot.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int squareRoot(int A) {
+        return sqrt(A);
+}
+int main () {
+        int A;
+        cin >> A;
+        cout << squareRoot(A);
+	return 0;
+}
