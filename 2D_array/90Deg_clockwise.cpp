288b6dbfd0ba15176abf1f1453cc7e191025bb6d
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-03-26 13:56:05 +0530

    rotate matrix 90 deg clock wise

--- 2D_array/90Deg_clockwise.cpp
+++ 2D_array/90Deg_clockwise.cpp
@@ -11,13 +11,20 @@ int main() {
     };
 
     int n = matrix.size();
-
+      vector<vector<int>>ans(n,vector<int>(n));
+      int k;
     for(int j = 0; j < n; j++) {
+        k=0;
         for(int i = n - 1; i >= 0; i--) {
-            cout << matrix[i][j] << " ";
+           ans[j][k]= matrix[i][j];
+            k++;
         }
-        cout << endl;
     }
-
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
